4b7fc2a1e30cb40ee0811460e96267b13cdb6d1b
Eric Promislow <eric.promislow@suse.com>
2023-12-19 14:01:14 -0800

    Remove debug stmts

--- build/script.js
+++ build/script.js
@@ -7,14 +7,17 @@ let view = null;
 let model = view;
 
 function afdCheck() {
-    console.log('in afdCheck ...');
-    console.table(location);
+    // console.log('in afdCheck ...');
+    // console.table(location);
     const date = new Date();
-    // if (date.getMonth() !== 3 || date.getDate() !== 1) {
-    if (date.getMonth() !== 11 || date.getDate() !== 19) {
+    if (date.getMonth() !== 3 || date.getDate() !== 1) {
+    //if (date.getMonth() !== 11 || date.getDate() !== 19) {
         const url = location.href;
         const url2 = url.replace('41', '');
-        console.log(`Fixed url: <${ url }>`);
+        if (url2 === url) {
+            return;
+        }
+        // console.log(`Fixed url: <${ url }>`);
         window.location.href = url2;
     }
 }
